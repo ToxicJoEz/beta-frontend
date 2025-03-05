3b921defeb057f70edd00ac872d39ccdb266e86b
ToxicJoEz <latenightgamer20166@gmail.com>
2025-03-05 17:26:48 +0200

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
@@ -6,20 +7,26 @@ export default function Footer() {
       <footer className="bg-white rounded-lg shadow-sm dark:bg-gray-900 m-4">
         <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
           <div className="sm:flex sm:items-center sm:justify-between">
-            <a
-              href="https://flowbite.com/"
-              className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
+            <Link
+              to="/"
+              className="flex items-center space-x-3 rtl:space-x-reverse"
             >
               <img
-                src="https://flowbite.com/docs/images/logo.svg"
-                className="h-8"
+                src="https://i.imgur.com/fzfxOQl.png"
+                className="h-12"
                 alt="Flowbite Logo"
               />
               <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-                Flowbite
+                File Flow
               </span>
-            </a>
+            </Link>
+
             <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+              <li>
+                <a href="#" className="hover:underline me-4 md:me-6">
+                  Home
+                </a>
+              </li>
               <li>
                 <a href="#" className="hover:underline me-4 md:me-6">
                   About
@@ -31,11 +38,6 @@ export default function Footer() {
                 </a>
               </li>
               <li>
-                <a href="#" className="hover:underline me-4 md:me-6">
-                  Licensing
-                </a>
-              </li>
-              <li>
                 <a href="#" className="hover:underline">
                   Contact
                 </a>
@@ -44,11 +46,7 @@ export default function Footer() {
           </div>
           <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
           <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
-            © 2023{"{"}" "{"}"}
-            <a href="https://flowbite.com/" className="hover:underline">
-              Flowbite™
-            </a>
-            . All Rights Reserved.
+            © 2025 File Flow. All Rights Reserved.
           </span>
         </div>
       </footer>
